6a71973d1cea13916726845640f77be872d9674e
Administrator <Administrator@192.168.10.91>
2018-07-19 16:21:03 +0800

    new project to study appium

---
+++ src/main/java/gby/appium/ui/App.java
@@ -0,0 +1,13 @@
+package gby.appium.ui;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
